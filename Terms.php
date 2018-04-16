530cf8a7665feeb75fc7a962df8e0bbfcfef325d
Grimes Enda <B00664037@scm.net>
2018-04-16 12:44:17 +0100

    terms and conditions

---
+++ Terms.php
@@ -0,0 +1,154 @@
+<?php
+include('functions.php');
+?>
+<!DOCTYPE html>
+ <html class="no-js"> 
+	<head>
+	<meta charset="utf-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<title>RG Fitness</title>
+
+ 
+
+	<!-- Place favicon.ico and apple-touch-icon.png in the root directory -->
+	<link rel="icon" href="images\RG_Fitness_logo.jpg" type="image/jpg" sizes="16x16">
+
+	<link href='https://fonts.googleapis.com/css?family=Roboto:400,100,300,700,900' rel='stylesheet' type='text/css'>
+	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/meyer-reset/2.0/reset.min.css">
+	<link rel='stylesheet prefetch' href='https://cdn.jsdelivr.net/jquery.slick/1.5.9/slick.css'>
+	
+	
+	<!-- Animate.css -->
+	<link rel="stylesheet" href="css/animate.css">
+	<!-- Icomoon Icon Fonts-->
+	<link rel="stylesheet" href="css/icomoon.css">
+	<!-- Bootstrap  -->
+	<link rel="stylesheet" href="css/bootstrap.css">
+	<!-- Superfish -->
+	<link rel="stylesheet" href="css/superfish.css">
+	<link rel="stylesheet" href="css/style.css">
+	<link rel="stylesheet" href="css/slider.css">
+	
+
+	<!-- Modernizr JS -->
+	<script src="js/modernizr-2.6.2.min.js"></script>
+	
+	
+	</head>
+	<body>
+		<div id="fh5co-wrapper">
+		<div id="fh5co-page">
+		<div id="fh5co-header">
+			<header id="fh5co-header-section">
+				<div class="container">
+					<div class="nav-header">
+						<a href="#" class="js-fh5co-nav-toggle fh5co-nav-toggle"><i></i></a>
+						<h1 id="fh5co-logo"><a href="index.php">RG<span>Fitness</span></a></h1>
+						<!-- Navigation -->
+						<nav id="fh5co-menu-wrap" role="navigation">
+							<ul class="sf-menu" id="fh5co-primary-menu">
+								<li class="active">
+									<a href="index.php">Home</a>
+								</li>
+								<li>
+									<a href="classes.php" class="fh5co-sub-ddown">Classes</a>
+									 
+								</li>
+								<li>
+									<a href="tutorials.php">Tutorials</a>
+								</li>
+								<li><a href="trainer.php">Trainers</a></li>
+								<li><a href="shop.php">Shop</a></li>
+								<li><a href="contact.php">Contact</a></li>
+								<li><a href="login.php">Login</a></li>
+								<li><a href="register.php">Register</a></li>
+							</ul>
+						</nav>
+					</div>
+				</div>
+			</header>		
+		</div>
+		
+		
+		<!-- header -->
+		<div class="fh5co-parallax" style="background-image: url(images/home-deadlift.jpg);" data-stellar-background-ratio="0.5">
+			<div class="overlay"></div>
+			<div class="container">
+				<div class="row">
+					<div class="col-md-8 col-md-offset-2 col-sm-12 col-sm-offset-0 col-xs-12 col-xs-offset-0 text-center fh5co-table">
+						<div class="fh5co-intro fh5co-table-cell animate-box">
+							<h1 class="text-center">Terms and Conditions</h1>
+							
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+		
+		<!--Terms and Conditions-->
+		<div id="fh5co-schedule-section" class="fh5co-lightgray-section">
+			<div class="container">
+				<div class="row">
+					<div class="col-md-8 col-md-offset-2">
+						<div class="heading-section text-center animate-box">
+							<h1>Terms and Conditions</h1>
+							<h3>1.Registration</h3>
+							<p>1.1 You can browse the site without registering for membership. To have full access to the site you will need to be a paying member. We reserve the right to cancel your membership at any time at our absolute discretion.</p>
+							<p>1.2 To be able to register you must:</p>
+							<p>1.2.1 be 16 years or over; and</p>
+							<p>1.2.2 have a valid payment method.</p>
+							<p>1.3 You may terminate your membership at any time but you will not be entitled to any refund of any memberships fees paid.</p>
+							<h3>2.Biling</h3>
+							<p>2.1 Membership can only paid via a direct debit.</p>
+							<P>2.2 Money will be taken out on a monthly basis.</P>
+							<p>2.3 Cancel anytime</p>
+							<p>2.4 Once signed up, you will be able to gain access to the gym via finger print scanner at front door.</p>
+							<h3>3.Cancellation</h3>
+							<p>3.1 You can cancel your membership at any time and no further payments will be taken.</p>
+							<p>3.2 To cancel please either contact Ruari directly or stop all payments from your direct debit. We will not be held responsible for payments if the correct procedure is not followed.</p>
+						</div>
+					</div>
+				</div>
+				
+					
+
+						
+			</div>
+		</div>
+
+		
+		<?php include 'footer.php';?>
+
+	</div>
+	<!-- END fh5co-page -->
+
+	</div>
+	<!-- END fh5co-wrapper -->
+	
+	<!-- jQuery -->
+	
+
+	<script src="js/jquery.min.js"></script>
+	<!-- jQuery Easing -->
+	<script src='http://cdnjs.cloudflare.com/ajax/libs/jquery-easing/1.3/jquery.easing.min.js'></script>
+	<script src='http://cdn.jsdelivr.net/jquery.slick/1.5.9/slick.min.js'></script>
+	<!-- Bootstrap -->
+	<script src="js/bootstrap.min.js"></script>
+	<!-- Waypoints -->
+	<script src="js/jquery.waypoints.min.js"></script>
+	<!-- Stellar -->
+	<script src="js/jquery.stellar.min.js"></script>
+	<!-- Superfish -->
+	<script src="js/hoverIntent.js"></script>
+	<script src="js/superfish.js"></script>
+
+	<!-- Main JS (Do not remove) -->
+	<script src="js/main.js"></script>
+	
+	
+		<script src="js/slider.js"></script>
+
+	
+	</body>
+</html>
+
