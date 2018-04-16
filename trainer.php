058d6defd70c2c61615ccd23cd5143962458f9d3
Grimes Enda <B00664037@scm.net>
2018-04-16 13:05:08 +0100

    trainer page

---
+++ trainer.php
@@ -0,0 +1,224 @@
+<?php
+include('functions.php');
+?>
+<!DOCTYPE html>
+<html class="no-js"> 
+	<head>
+	<meta charset="utf-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<title>RG Fitness</title>
+	
+	<!-- Place favicon.ico and apple-touch-icon.png in the root directory -->
+	<link rel="icon" href="images\RG_Fitness_logo.jpg" type="image/jpg" sizes="16x16">
+
+	<link href='https://fonts.googleapis.com/css?family=Roboto:400,100,300,700,900' rel='stylesheet' type='text/css'>
+	
+	<!-- Animate.css -->
+	<link rel="stylesheet" href="css/animate.css">
+	<!-- Icomoon Icon Fonts-->
+	<link rel="stylesheet" href="css/icomoon.css">
+	<!-- Bootstrap  -->
+	<link rel="stylesheet" href="css/bootstrap.css">
+	<!-- Superfish -->
+	<link rel="stylesheet" href="css/superfish.css">
+
+	<link rel="stylesheet" href="css/style.css">
+
+
+	<!-- Modernizr JS -->
+	<script src="js/modernizr-2.6.2.min.js"></script>
+	
+
+	</head>
+	<!-- Logo -->
+	<body>
+		<div id="fh5co-wrapper">
+		<div id="fh5co-page">
+		<div id="fh5co-header">
+			<header id="fh5co-header-section">
+				<div class="container">
+					<div class="nav-header">
+						<a href="#" class="js-fh5co-nav-toggle fh5co-nav-toggle"><i></i></a>
+						<h1 id="fh5co-logo"><a href="index.php">RG<span>Fitness</span></a></h1>
+						<!-- Navigation -->
+						<nav id="fh5co-menu-wrap" role="navigation">
+							<ul class="sf-menu" id="fh5co-primary-menu">
+								<li class="active">
+									<a href="index.php">Home</a>
+								</li>
+								<li>
+									<a href="classes.php" class="fh5co-sub-ddown">Classes</a>
+									 
+								</li>
+								<li>
+									<a href="tutorials.php">Tutorials</a>
+								</li>
+								<li><a href="trainer.php">Trainers</a></li>
+								<li><a href="shop.php">Shop</a></li>
+								<li><a href="contact.php">Contact</a></li>
+								<li><a href="login.php">Login</a></li>
+								<li><a href="register.php">Register</a></li>
+							</ul>
+						</nav>
+					</div>
+				</div>
+			</header>		
+		</div>
+		<!-- end:fh5co-header -->
+		<div class="fh5co-parallax" style="background-image: url(images/Personal-Training.jpg);" data-stellar-background-ratio="0.5">
+			<div class="overlay"></div>
+			<div class="container">
+				<div class="row">
+					<div class="col-md-8 col-md-offset-2 col-sm-12 col-sm-offset-0 col-xs-12 col-xs-offset-0 text-center fh5co-table">
+						<div class="fh5co-intro fh5co-table-cell animate-box">
+							<h1 class="text-center">Trainers</h1>
+							<p>Select anyone of our fantastic trainers.</a></p>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div><!-- end: fh5co-parallax -->
+		<!-- end:fh5co-hero -->
+		<div id="fh5co-team-section">
+			<div class="container">
+				<div class="row">
+					<div class="col-md-8 col-md-offset-2">
+						<div class="heading-section text-center animate-box">
+							<h2>Meet Our Trainers</h2>
+							<p>Below are the trainers and what they offer here at RG Fitness</p>
+						</div>
+					</div>
+				</div>
+				<div class="row text-center">
+					<div class="col-md-4 col-sm-6">
+						<div class="team-section-grid animate-box" style="background-image: url(images/ruari.jpg);">
+							<div class="overlay-section">
+								<div class="desc">
+									<h3>Ruari Grimes</h3>
+									<span>Head Trainer</span>
+									<p>Provide a wide range of Personal Training as well as GAA conditioning training.</p>
+									<p class="fh5co-social-icons">
+										<a href="https://twitter.com/Fitness_RG"><i class="icon-twitter2"></i></a>
+										<a href="https://www.facebook.com/RG-Fitness-1610164359233735/"><i class="icon-facebook2"></i></a>
+										<a href="https://www.instagram.com/rgfitness01/?hl=en"><i class="icon-instagram"></i></a>
+									</p>
+								</div>
+							</div>
+						</div>
+					</div>
+					<div class="col-md-4 col-sm-6">
+						<div class="team-section-grid animate-box" style="background-image: url(images/ailise.jpg);">
+							<div class="overlay-section">
+								<div class="desc">
+									<h3>Ailise Coyle</h3>
+									<span>Ladies Weights</span>
+									<p>Providing women young and old with the correct techniques for lifting weights</p>
+									<p class="fh5co-social-icons">
+										<a href="https://twitter.com/AiliseG"><i class="icon-twitter2"></i></a>
+										<a href="https://www.facebook.com/PTwithAiliseG/"><i class="icon-facebook2"></i></a>
+										<a href="https://www.instagram.com/ptwithailiseg/?hl=en"><i class="icon-instagram"></i></a>
+									</p>
+								</div>
+							</div>
+						</div>
+					</div>
+					<div class="col-md-4 col-sm-6">
+						<div class="team-section-grid animate-box" style="background-image: url(images/christine.jpg);">
+							<div class="overlay-section">
+								<div class="desc">
+									<h3>Christine Penrose</h3>
+									<span>Body Bump</span>
+									<p>Specializes in Body Bump to help strengthen and shape the entire body.</p>
+									<p class="fh5co-social-icons">
+										<a href="https://twitter.com/Fitness_RG"><i class="icon-twitter2"></i></a>
+										<a href="https://www.facebook.com/RG-Fitness-1610164359233735/"><i class="icon-facebook2"></i></a>
+										<a href="https://www.instagram.com/rgfitness01/?hl=en"><i class="icon-instagram"></i></a>
+									</p>
+								</div>
+							</div>
+						</div>
+					</div>
+					<div class="col-md-4 col-sm-6">
+						<div class="team-section-grid animate-box" style="background-image: url(images/trainer-4.jpg);">
+							<div class="overlay-section">
+								<div class="desc">
+									<h3>John McElroy</h3>
+									<span>Boxercise</span>
+									<p>Boxercise, a safe, effective and fun form of exercise suitable for all ages and levels of ability.</p>
+									<p class="fh5co-social-icons">
+										<a href="https://twitter.com/Fitness_RG"><i class="icon-twitter2"></i></a>
+										<a href="https://www.facebook.com/RG-Fitness-1610164359233735/"><i class="icon-facebook2"></i></a>
+										<a href="https://www.instagram.com/rgfitness01/?hl=en"><i class="icon-instagram"></i></a>
+									</p>
+								</div>
+							</div>
+						</div>
+					</div>
+					<div class="col-md-4 col-sm-6">
+						<div class="team-section-grid animate-box" style="background-image: url(images/trainer-5.jpg);">
+							<div class="overlay-section">
+								<div class="desc">
+									<h3>Elaine Kelly</h3>
+									<span>Pilates</span>
+									<p>Offers two sets of Pilates classes for both men and women.</p>
+									<p class="fh5co-social-icons">
+										<a href="https://twitter.com/Fitness_RG"><i class="icon-twitter2"></i></a>
+										<a href="https://www.facebook.com/RG-Fitness-1610164359233735/"><i class="icon-facebook2"></i></a>
+										<a href="https://www.instagram.com/rgfitness01/?hl=en"><i class="icon-instagram"></i></a>
+									</p>
+								</div>
+							</div>
+						</div>
+					</div>
+						
+				</div>
+			</div>
+		</div>
+		<div id="fh5co-programs-section" class="fh5co-lightgray-section">
+			<div class="container">
+				<div class="row">
+					<div class="col-md-8 col-md-offset-2">
+						<div class="heading-section text-center animate-box">
+							<h2>Select Trainer</h2>
+							<p>Now that you are familar with our trainers, by clicking the link you can schedule your Personal Training session with your prefered Personal Trainer. Make sure to Login/Register before booking your Personal Trainer. </p>
+							<span><a href="PTform.php" class="btn btn-primary">Select Trainer </a></span>
+						</div>
+					</div>
+				</div>
+				
+
+				</div>
+			</div>
+		</div>
+		
+		<?php include 'footer.php';?>
+		
+
+	</div>
+	<!-- END fh5co-page -->
+
+	</div>
+	<!-- END fh5co-wrapper -->
+
+	<!-- jQuery -->
+
+
+	<script src="js/jquery.min.js"></script>
+	<!-- jQuery Easing -->
+	<script src="js/jquery.easing.1.3.js"></script>
+	<!-- Bootstrap -->
+	<script src="js/bootstrap.min.js"></script>
+	<!-- Waypoints -->
+	<script src="js/jquery.waypoints.min.js"></script>
+	<!-- Stellar -->
+	<script src="js/jquery.stellar.min.js"></script>
+	<!-- Superfish -->
+	<script src="js/hoverIntent.js"></script>
+	<script src="js/superfish.js"></script>
+
+	<!-- Main JS (Do not remove) -->
+	<script src="js/main.js"></script>
+
+	</body>
+</html>
+
