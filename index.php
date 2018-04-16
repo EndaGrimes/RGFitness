492ccd8cc961881d15d59cd07d9f9b436c857b87
Grimes Enda <B00664037@scm.net>
2018-04-16 12:35:50 +0100

    index

---
+++ index.php
@@ -0,0 +1,185 @@
+<?php
+include('functions.php');
+?>
+<!DOCTYPE html>
+ <html class="no-js"> 
+	<head>
+	<meta charset="utf-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<title>RG Fitness</title>
+
+ 
+
+	<!-- Place favicon.ico and apple-touch-icon.png in the root directory -->
+	<link rel="icon" href="images\RG_Fitness_logo.jpg" type="image/jpg" sizes="16x16">
+
+	<link href='https://fonts.googleapis.com/css?family=Roboto:400,100,300,700,900' rel='stylesheet' type='text/css'>
+	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/meyer-reset/2.0/reset.min.css">
+	<link rel='stylesheet prefetch' href='https://cdn.jsdelivr.net/jquery.slick/1.5.9/slick.css'>
+	
+	
+	<!-- Animate.css -->
+	<link rel="stylesheet" href="css/animate.css">
+	<!-- Icomoon Icon Fonts-->
+	<link rel="stylesheet" href="css/icomoon.css">
+	<!-- Bootstrap  -->
+	<link rel="stylesheet" href="css/bootstrap.css">
+	<!-- Superfish -->
+	<link rel="stylesheet" href="css/superfish.css">
+	<link rel="stylesheet" href="css/style.css">
+	<link rel="stylesheet" href="css/slider.css">
+	
+
+	<!-- Modernizr JS -->
+	<script src="js/modernizr-2.6.2.min.js"></script>
+	
+	
+	</head>
+	<body>
+		<div id="fh5co-wrapper">
+		<div id="fh5co-page">
+		<div id="fh5co-header">
+			<header id="fh5co-header-section">
+				<div class="container">
+					<div class="nav-header">
+						<a href="#" class="js-fh5co-nav-toggle fh5co-nav-toggle"><i></i></a>
+						<h1 id="fh5co-logo"><a href="index.php">RG<span>Fitness</span></a></h1>
+						<!-- Navigation -->
+						<nav id="fh5co-menu-wrap" role="navigation">
+							<ul class="sf-menu" id="fh5co-primary-menu">
+								<li class="active">
+									<a href="index.php">Home</a>
+								</li>
+								<li>
+									<a href="classes.php" class="fh5co-sub-ddown">Classes</a>
+									 
+								</li>
+								<li>
+									<a href="tutorials.php">Tutorials</a>
+								</li>
+								<li><a href="trainer.php">Trainers</a></li>
+								<li><a href="shop.php">Shop</a></li>
+								<li><a href="contact.php">Contact</a></li>
+								<li><a href="login.php">Login</a></li>
+								<li><a href="register.php">Register</a></li>
+								
+							</ul>
+						</nav>
+					</div>
+				</div>
+			</header>		
+		</div>
+		
+		
+		<!-- header -->
+		<div class="fh5co-hero">
+			<div class="fh5co-overlay"></div>
+			<div class="fh5co-cover" data-stellar-background-ratio="0.5" style="background-image: url(images/home.jpg);">
+				<div class="desc animate-box">
+					<div class="container">
+						<div class="row">
+							
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+		<!-- About Us -->
+		<div id="fh5co-schedule-section" class="fh5co-lightgray-section">
+			<div class="container">
+				<div class="row">
+					<div class="col-md-8 col-md-offset-2">
+						<div class="heading-section text-center animate-box">
+							<h2>About Us</h2>
+							<p>Here at RG Fitness we offer our customers the best information, the best equipment and the best trainers for the best results.
+							RG Fitness is committed to offering top class gym and fitness class facilities at a fraction of the usual cost of health club membership, by removing the unnecessary frills.
+							Our expert trainers will help you plan your fitness goals and give you the tools and techniques you need to succeed.</p>
+						</div>
+					</div>
+				</div>
+				
+					
+
+						
+			</div>
+		</div>
+			
+		
+		<div class="fh5co-parallax" style="background-image: url(images/gym-homepage.jpg);" data-stellar-background-ratio="0.5">
+			<div class="overlay"></div>
+			<div class="container">
+				<div class="row">
+					<div class="col-md-8 col-md-offset-2 col-sm-12 col-sm-offset-0 col-xs-12 col-xs-offset-0 text-center fh5co-table">
+						<div class="fh5co-intro fh5co-table-cell animate-box">
+							<h1 class="text-center">Train like a champion</h1>
+							<p>RG Fitness will revolutionise the way you train. Our state of the art facilities will help you perfect your skills and get the best results possible.</p>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+		<!-- end: fh5co-parallax -->
+		
+		<div id="fh5co-programs-section">
+			<div class="container">
+				<div class="row">
+					<div class="col-md-8 col-md-offset-2">
+						<div class="heading-section text-center animate-box">
+							<h2>Our Partners</h2>
+							<img src="images\blkbox.jpg" alt="blkbox logo" style="width="050" height="100";">
+							<br></br>
+							<img src="images\ONeills.jpg" alt="oneills logo" style="width="050" height="100";">
+							<br></br>
+							<img src="images\goldwhey.jpg" alt="oneills logo" style="width="050" height="100";">
+							
+						</div>
+					</div>
+				</div>
+				
+			</div>
+		</div>
+		
+		<?php include 'footer.php';?>
+					
+					
+					
+					
+		
+		
+		
+		
+	
+
+	</div>
+	<!-- END fh5co-page -->
+
+	</div>
+	<!-- END fh5co-wrapper -->
+	
+	<!-- jQuery -->
+	
+
+	<script src="js/jquery.min.js"></script>
+	<!-- jQuery Easing -->
+	<script src='http://cdnjs.cloudflare.com/ajax/libs/jquery-easing/1.3/jquery.easing.min.js'></script>
+	<script src='http://cdn.jsdelivr.net/jquery.slick/1.5.9/slick.min.js'></script>
+	<!-- Bootstrap -->
+	<script src="js/bootstrap.min.js"></script>
+	<!-- Waypoints -->
+	<script src="js/jquery.waypoints.min.js"></script>
+	<!-- Stellar -->
+	<script src="js/jquery.stellar.min.js"></script>
+	<!-- Superfish -->
+	<script src="js/hoverIntent.js"></script>
+	<script src="js/superfish.js"></script>
+
+	<!-- Main JS (Do not remove) -->
+	<script src="js/main.js"></script>
+	
+	
+		<script src="js/slider.js"></script>
+
+	
+	</body>
+</html>
+
