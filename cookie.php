39dec6290b3d997933239aace707b2cc7d8cdd09
Grimes Enda <B00664037@scm.net>
2018-04-18 18:07:49 +0100

    cookie policy

---
+++ cookie.php
@@ -0,0 +1,121 @@
+<?php
+include('functions.php');
+?>
+<!DOCTYPE html>
+ <html class="no-js">
+	<head>
+	<meta charset="utf-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<title>RG Fitness</title>
+
+
+
+	<!-- Place favicon.ico and apple-touch-icon.png in the root directory -->
+	<link rel="icon" href="images\RG_Fitness_logo.jpg" type="image/jpg" sizes="16x16">
+
+	<link href='https://fonts.googleapis.com/css?family=Roboto:400,100,300,700,900' rel='stylesheet' type='text/css'>
+	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/meyer-reset/2.0/reset.min.css">
+	<link rel='stylesheet prefetch' href='https://cdn.jsdelivr.net/jquery.slick/1.5.9/slick.css'>
+
+
+	<!-- Animate.css -->
+	<link rel="stylesheet" href="css/animate.css">
+	<!-- Icomoon Icon Fonts-->
+	<link rel="stylesheet" href="css/icomoon.css">
+	<!-- Bootstrap  -->
+	<link rel="stylesheet" href="css/bootstrap.css">
+	<!-- Superfish -->
+	<link rel="stylesheet" href="css/superfish.css">
+	<link rel="stylesheet" href="css/style.css">
+	<link rel="stylesheet" href="css/slider.css">
+
+
+	<!-- Modernizr JS -->
+	<script src="js/modernizr-2.6.2.min.js"></script>
+
+
+	</head>
+	<body>
+		<div id="fh5co-wrapper">
+		<div id="fh5co-page">
+		<div id="fh5co-header">
+			<?php include 'header.php';?>
+		</div>
+
+
+		<!-- header -->
+		<div class="fh5co-parallax" style="background-image: url(images/home-deadlift.jpg);" data-stellar-background-ratio="0.5">
+			<div class="overlay"></div>
+			<div class="container">
+				<div class="row">
+					<div class="col-md-8 col-md-offset-2 col-sm-12 col-sm-offset-0 col-xs-12 col-xs-offset-0 text-center fh5co-table">
+						<div class="fh5co-intro fh5co-table-cell animate-box">
+							<h1 class="text-center">Cookie Policy</h1>
+
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+
+		<!--Cookie Policy-->
+		<div id="fh5co-schedule-section" class="fh5co-lightgray-section">
+			<div class="container">
+				<div class="row">
+					<div class="col-md-8 col-md-offset-2">
+						<div class="heading-section text-center animate-box">
+							<h1>Cookie Information</h1>
+							<h3>What is a Cookie?</h3>
+							<p>A cookie, also known as a browser cookie, is a text file containing small amounts of information which a server may download to your computer, mobile or tablet when you visit a website or use an app.</p>
+
+							<p>There are different types of cookies which are used to do different things, such as letting you navigate between different pages on a website efficiently, remembering preferences you have given and helping us to identify ways to improve your overall site experience. Others are used to provide you with advertising which is more tailored to your interests or to measure the number of site visits and the most popular pages users visit</p>
+
+							<h3>How does RG Fitness use cookies?</h3>
+							<p>RG Fitness only uses browser cookies to measure non-personal information, for example to learn about the behaviour of visitors to our website and how they respond to our marketing communications. The more we learn, the better we are able to provide relevant and interesting content and services. The first person cookies set by RG Fitness do not contain any personally identifiable information.</p>
+
+						</div>
+					</div>
+				</div>
+
+
+
+
+			</div>
+		</div>
+
+
+		<?php include 'footer.php';?>
+
+
+	</div>
+	<!-- END fh5co-page -->
+
+	</div>
+	<!-- END fh5co-wrapper -->
+
+	<!-- jQuery -->
+
+
+	<script src="js/jquery.min.js"></script>
+	<!-- jQuery Easing -->
+	<script src='http://cdnjs.cloudflare.com/ajax/libs/jquery-easing/1.3/jquery.easing.min.js'></script>
+	<script src='http://cdn.jsdelivr.net/jquery.slick/1.5.9/slick.min.js'></script>
+	<!-- Bootstrap -->
+	<script src="js/bootstrap.min.js"></script>
+	<!-- Waypoints -->
+	<script src="js/jquery.waypoints.min.js"></script>
+	<!-- Stellar -->
+	<script src="js/jquery.stellar.min.js"></script>
+	<!-- Superfish -->
+	<script src="js/hoverIntent.js"></script>
+	<script src="js/superfish.js"></script>
+
+	<!-- Main JS (Do not remove) -->
+	<script src="js/main.js"></script>
+
+
+		<script src="js/slider.js"></script>
+
+
+	</body>
+</html>
